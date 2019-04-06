2a11e7648309b4b3be58f1448d2f90f5265bde63
Mffadmin <794459843@qq.com>
2019-04-06 17:54:42 +0800

    初始化

---
+++ NeverUp/src/main/java/com/it/up/NeverUpApplication.java
@@ -0,0 +1,13 @@
+package com.it.up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeverUpApplication.class, args);
+	}
+
+}
